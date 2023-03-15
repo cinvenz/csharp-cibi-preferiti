a782d583f4692fde03d2f4663f0ba52f6580d765
cinvenz <vincenzobortune@gmail.com>
2023-03-15 18:01:49 +0100

    fix bonus

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] food = { "pizza", "carbonara", "spinaci", "frittata", "hamburger", };
+﻿string[] food = { "pizza", "carbonara", "spinaci", "frittata", "hamburger", "patate"};
 Console.WriteLine("lunghezza classifica:" + " " + food.Length + " " + "posizioni");
 
 for (int i = 0; i < food.Length; i++)
@@ -11,4 +11,11 @@ Console.WriteLine("Cibo in prima posizione:" + " " + food[1]);
 
 Console.WriteLine("Cibo preferito in ultima posizione:" + " " + food[food.Length -1]);
 
-Console.WriteLine("Cibo a metà classifica:" + " " + food[food.Length / 2]);
+if (food.Length % 2 == 0)
+{
+    Console.WriteLine("Cibo a metà classifica:" + " " + food[food.Length / 2 - 1] + " e " + food[food.Length / 2]);
+}
+else
+{
+    Console.WriteLine("Cibo a metà classifica:" + " " + food[food.Length / 2]);
+}
