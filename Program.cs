4f0d9371bcb560517e981ac2aee23a9f4ee27657
cinvenz <vincenzobortune@gmail.com>
2023-03-15 16:17:51 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string[] food = { "pizza", "carbonara", "spinaci", "frittata", "hamburger", };
+Console.WriteLine("lunghezza classifica:" + " " + food.Length + " " + "posizioni");
+
+for (int i = 0; i < food.Length; i++)
+{
+    Console.WriteLine((i) + " " + food[i]);
+}
+
+
+Console.WriteLine("Cibo in prima posizione:" + " " + food[1]);
+
+Console.WriteLine("Cibo preferito in ultima posizione:" + " " + food[2]);
+
+Console.WriteLine("Cibo a metà classifica:" + " " + food[food.Length / 2]);
